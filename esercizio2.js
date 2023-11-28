073902f07be45b081ef4fbc4848a9883798b5cd8
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2023-11-28 20:23:15 +0100

    secondo commmit es 2

--- esercizio2.js
+++ esercizio2.js
@@ -2,15 +2,22 @@ let agenda = {
     contatto : [],
 
     stampaAgenda: function(){
-        console.log(this.contatto);
+        var lista = '';
+        var indiceNuovo;
+        for(let i=0;i<this.contatto.length;i++){
+            indiceNuovo = i+1;
+            lista = lista+" "+indiceNuovo+" - Nome: "+this.contatto[i].nome+"  Numero: "+this.contatto[i].numeroTelefono+"\n";
+        }
+        alert(lista);
     },
 
     stampaSingolo: function(indice){
-        console.log(this.contatto[indice]);
+        var contatto = "Nome: "+this.contatto[indice-1].nome+"  Numero: "+this.contatto[indice-1].numeroTelefono;
+        alert(contatto);
     },
 
     eliminaContatto: function(indice){
-        this.contatto.splice(indice, 1);
+        this.contatto.splice(indice-1, 1);
     },
 
     aggiungiContatto: function(nome, numeroTelefono){
@@ -22,7 +29,10 @@ let agenda = {
     },
 
     modificaContatto: function(indice){
-        
+        let nome = prompt("Inserisci nuovo nome");
+        let numeroTelefono = parseInt(prompt("Inserisci nuovo numero"));
+        this.contatto[indice-1].nome=nome;
+        this.contatto[indice-1].numeroTelefono=numeroTelefono;
     }
 }
 
@@ -30,23 +40,23 @@ let operazione = 1;
 
 while(operazione>0){
     operazione = parseInt(prompt("1:Leggi agenda\n2:Leggi contatto\n3:Elimina contatto\n4:Aggiungi contatto\n5:Modifica contatto\n0:Esci"));
-    if(operazione=1){
+    if(operazione==1){
         agenda.stampaAgenda();
     }
-    else if(operazione=2){
+    else if(operazione==2){
         let indice = parseInt(prompt("Inserisci indice contatto"));
         agenda.stampaSingolo(indice);
     }
-    else if(operazione=3){
+    else if(operazione==3){
         let indice = parseInt(prompt("Inserisci indice contatto da eliminare"));
         agenda.eliminaContatto(indice);
     }
-    else if(operazione=4){
+    else if(operazione==4){
         let nome = prompt("Inserisci nome");
         let numeroTelefono = parseInt(prompt("Inserisci numero"));
         agenda.aggiungiContatto(nome, numeroTelefono);
     }
-    else if(operazione=5){
+    else if(operazione==5){
         let indice = parseInt(prompt("Inserisci indice contatto da modificare"));
         agenda.modificaContatto(indice);
     }
